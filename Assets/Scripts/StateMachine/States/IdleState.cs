f0e733da640ddc288d8c771f9b77167366b02873
Anunakia4 <gtfalen@gmail.com>
2023-08-26 18:30:20 +0600

    Added character animations

--- Assets/Scripts/StateMachine/States/IdleState.cs
+++ Assets/Scripts/StateMachine/States/IdleState.cs
@@ -4,6 +4,11 @@ namespace StateMachine.States
 {
     public class IdleState: BaseState
     {
-        
+        [SerializeField] private Animator _animator;
+
+        public override void OnEnterState()
+        {
+            _animator.Play("Idle");
+        }
     }
 }
--- Assets/Scripts/StateMachine/States/RunState.cs
+++ Assets/Scripts/StateMachine/States/RunState.cs
@@ -4,6 +4,12 @@ namespace StateMachine.States
 {
     public class RunState: BaseState
     {
-        
+        [SerializeField] private Animator _animator;
+
+        public override void OnEnterState()
+        {
+            _animator.speed = 1.5f;
+            _animator.Play("Walking");
+        }
     }
 }
